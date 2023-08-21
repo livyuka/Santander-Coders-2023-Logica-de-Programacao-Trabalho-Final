e0f3e2aca7866a8599635a7c94e1518bfac15dea
livia-tanaka <livia.tanaka@hotmail.com>
2023-08-21 14:30:40 -0300

    remove dois pontos da url do switch case

--- js/func-verificapalavra.js
+++ js/func-verificapalavra.js
@@ -109,25 +109,25 @@ function comparaListas() {
 function imagemMario() {
     switch(tentativas){
         case 5:
-            document.getElementById("imagem").style.backgroundImage = "url('../img/mario-02.png')";
+            document.getElementById("imagem").style.backgroundImage = "url('./img/mario-02.png')";
             break;
         case 4:
-            document.getElementById("imagem").style.backgroundImage = "url('../img/mario-03.png')";
+            document.getElementById("imagem").style.backgroundImage = "url('./img/mario-03.png')";
             break;
         case 3:
-            document.getElementById("imagem").style.backgroundImage = "url('../img/mario-04.png')";
+            document.getElementById("imagem").style.backgroundImage = "url('./img/mario-04.png')";
             break;
         case 2:
-            document.getElementById("imagem").style.backgroundImage = "url('../img/mario-05.png')";
+            document.getElementById("imagem").style.backgroundImage = "url('./img/mario-05.png')";
             break;
         case 1:
-            document.getElementById("imagem").style.backgroundImage = "url('../img/mario-06.png')";
+            document.getElementById("imagem").style.backgroundImage = "url('./img/mario-06.png')";
             break;
         case 0:
-            document.getElementById("imagem").style.backgroundImage = "url('../img/mario-07.png')";
+            document.getElementById("imagem").style.backgroundImage = "url('./img/mario-07.png')";
             break;
         default:
-            document.getElementById("imagem").style.backgroundImage = "url('../img/mario-01.png')";
+            document.getElementById("imagem").style.backgroundImage = "url('./img/mario-01.png')";
             break;
     }
 }
